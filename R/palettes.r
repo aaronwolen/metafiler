6ce4f5947047bdbedec9a45a4d88b5e2482d7db9
Aaron Wolen <aaron@wolen.com>
2016-07-06 12:10:26 -0400

    Add manual_pal_plus

--- R/palettes.r
+++ R/palettes.r
@@ -21,7 +21,20 @@
   )
 )
 
+#' Augmented color palette functions
+#'
+#' Palette functions return a function that generates colors. These
+#' \code{_plus()} functions are designed to work like their counterparts in the
+#' \code{scales} package, with the exception that additional standard colors are
+#' appended to the original palettes. The additional colors are not pretty but
+#' some effort has been made to avoid overly similar colors.
+#' @inheritParams scales::brewer_pal
+#' @inheritParams scales::manual_pal
+#' @name palettes
+NULL
 
+#' @export
+#' @rdname palettes
 brewer_pal_plus <- function(palette = 1, direction = 1) {
   palettes <- RColorBrewer::brewer.pal.info
 
@@ -38,6 +51,15 @@ brewer_pal_plus <- function(palette = 1, direction = 1) {
     brew.n <- palettes[pal, "maxcolors"]
     colors <- RColorBrewer::brewer.pal(brew.n, pal)
     if (direction == -1) colors <- rev(colors)
+    c(colors, .colors)[seq_len(n)]
+  }
+}
+
+#' @export
+#' @rdname palettes
+manual_pal_plus <- function(values) {
+  function(n) {
+    colors <- values
     c(colors, .colors)[seq_len(n)]
   }
 }
